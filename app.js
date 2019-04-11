6ada6b16b73df4d8f8c47e2e72dc53bb33c0332c
nishil <charsi@users.noreply.github.com>
2019-04-10 18:58:45 -0700

    add routes

--- app.js
+++ app.js
@@ -1,2 +1,40 @@
 "use strict"
 
+const express = require('express');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(helmet());
+app.use(bodyParser.json());
+
+const db = require('./lib/db')
+
+app.get('/api/customer/:id', async (req, res) => {
+  console.log(req.params,req.query);  
+});
+
+app.post('/api/customer/:id', async (req, res) => {
+  console.log(req.params,req.query);  
+});
+
+app.get('/api/customer/:id/name', async (req, res) => {
+  console.log(req.params,req.query);  
+});
+
+app.post('/api/customer/:id/:name', async (req, res) => {
+  console.log(req.params,req.query);  
+});
+
+app.get('/api/customer/:id/address', async (req, res) => {
+  console.log(req.params,req.query);  
+});
+
+app.post('/api/customer/:id/:address', async (req, res) => {
+  console.log(req.params,req.query);  
+});
+
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
