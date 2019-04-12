582f0e50208dd4b36e9af1a6d13e931c790f3108
nishil <charsi@users.noreply.github.com>
2019-04-12 13:55:45 -0700

    test: get by id

--- test.js
+++ test.js
@@ -1,9 +1,7 @@
-//During the test the env variable is set to test
 process.env.NODE_ENV = 'test';
 fs = require('fs')
 
 let nedb = require("nedb");
-//let Book = require('../app/models/book');
 
 //Require the dev-dependencies
 let chai = require('chai');
@@ -13,18 +11,22 @@ let should = chai.should();
 const db = require('./lib/db');
 
 chai.use(chaiHttp);
-//Our parent block
+
 describe('Customers', () => {
-    beforeEach(async () => { //Before each test we empty the database
-        await db.clearDb() 
-    });
 
 
-    /*
+  //Before each test, empty the database
+  beforeEach(async () => { 
+      await db.clearDb();
+  });
+
+
+  /*
   * Test the /POST route
   */
-  describe('/POST customer', () => {
-    it('it should create a new customer', (done) => {
+  describe('/POST customer', async () => {
+    //await db.clearDb();
+    it('it should create a new customer',  (done) => {
       let customer = {
         name: "Ryan",
         address: {
@@ -33,6 +35,7 @@ describe('Customers', () => {
           country: "US"
       }
     }
+    
       chai.request(app)
         .post('/api/customer')
         .send({customer:customer})
@@ -42,8 +45,10 @@ describe('Customers', () => {
             res.body.should.have.property('success');
             res.body.success.should.be.eql(true);
             res.body.should.have.property('customer');
+            res.body.customer.should.have.property('name');
             res.body.customer.name.should.be.eql(customer.name);
-            res.body.customer.address.should.be.eql(customer.address);
+            res.body.customer.should.have.property('address');
+            res.body.customer.address.should.be.eql(customer.address);            
           done();
         });
     });
@@ -52,15 +57,65 @@ describe('Customers', () => {
   /*
   * Test the /GET route
   */
- describe('/GET/ customer', () => {
-  it('it should GET all the customers', (done) => {
-    chai.request(app)
-    .get('/api/customer')
-    .end((err, res) => {
-          res.should.have.status(200);
-      done();
+  describe('/GET/ customer', () => {
+    it('it should GET all the customers', (done) => {
+      chai.request(app)
+      .get('/api/customer')
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.have.property('success');
+        res.body.success.should.be.eql(true);
+        res.body.should.have.property('customers');
+        done();
+      });
     });
   });
-});
+
+  describe('/GET/ customer', () => {
+    it('it should GET all the customers', (done) => {
+      chai.request(app)
+      .get('/api/customer')
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.have.property('success');
+        res.body.success.should.be.eql(true);
+        res.body.should.have.property('customers');
+        done();
+      });
+    });
+  });
+
+  describe('/GET/{custId} customer', async () => {
+    let customer = {name: "Ryan",address: {street_address: "999 Night Stalker Road",
+        postal_code: "12345",country: "US"}
+    }
+    it('it should GET customer by id', (done) => {
+      db.createCustomer(customer).then(()=>{
+        chai.request(app)
+        .get('/api/customer/1')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('success');
+          res.body.success.should.be.eql(true);
+          res.body.should.have.property('customer');
+          res.body.customer.should.have.property('name');
+          res.body.customer.name.should.be.eql(customer.name);
+          res.body.customer.should.have.property('address');
+          res.body.customer.address.should.be.eql(customer.address); 
+          done();
+        });
+      });
+    });
+  });
+
+  describe('/POST/:custId/delete customer', async () => {
+    let customer = {name: "Ryan",address: {street_address: "999 Night Stalker Road",
+        postal_code: "12345",country: "US"}
+    }
+    await db.createCustomer(customer);
+   
+  });
+  
 
 });
